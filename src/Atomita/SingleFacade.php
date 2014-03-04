8ac153d97ae9f381907de7e99a66838ee1ba4599
atomita <atom.at.omita@gmail.com>
2014-03-04 12:55:32 +0900

    version 0.0.2

---
+++ src/Atomita/SingleFacade.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Atomita;
+
+/**
+ * 
+ */
+class SimpleFacade extends Facade
+{
+
+	static protected function facadeInstance()
+	{
+		static $instans = array();
+
+		$class = '\\' . rtrim(get_called_class(), 'Facade');
+
+		if (!isset($instans[$class])){
+			$instans[$class] = new $class();
+		}
+		return $instans[$class];
+	}
+	
+}
